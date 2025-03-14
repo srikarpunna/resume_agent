541dfee897d5466589d262604ac1dea8e05a2509
srikarpunna <punna1s@cmich.edu>
2025-03-14 00:49:38 -0400

    fix: for dep

---
+++ index.js
@@ -0,0 +1,5 @@
+// This file exists to redirect to the actual server implementation
+// It's useful for deployment platforms that look for index.js in the root
+// 
+// Require and execute the actual server implementation
+require('./server/index.js');
